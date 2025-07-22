b10bb8893eba99c437cc691c16bf57e389e452ea
pilarvermu <pilaribermu@gmail.com>
2025-07-22 20:27:25 +0200

    Base de datos y rutas de prueba creadas

---
+++ blog-api/app.js
@@ -0,0 +1,16 @@
+// app.js
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API Blog funcionando");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en puerto ${PORT}`);
+});
