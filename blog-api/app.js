c095468f993d87610ba62a7c9f8e9058a8f45019
pilarvermu <pilaribermu@gmail.com>
2025-07-25 14:37:43 +0200

    Cambiado dominio de origen por vercel

--- blog-api/app.js
+++ blog-api/app.js
@@ -10,7 +10,7 @@ const cors = require("cors");
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // origen del frontend de Vite
+    origin: "https://blog-api-demo-lac.vercel.app/", // origen del frontend de Vite
     credentials: true,
   })
 );
